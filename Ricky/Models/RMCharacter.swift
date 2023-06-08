6df8997d069e6851b0304ebf8c7f07dc6dfb7493
DattaBiradar <dattathreya.biradar@influx.co.in>
2023-06-08 12:06:36 +0530

    Added Models

--- Ricky/Models/RMCharacter.swift
+++ Ricky/Models/RMCharacter.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+struct RMCharacter : Codable {
+    
+    let id : Int
+    let name: String
+    let status: RMCharacterStatus
+    let species : String
+    let type : String
+    let gender : RmCharacterGender
+    let origin: RMOrigin
+    let location: SingleLocation
+    let image: String
+    let episode: [ String]
+    let url: String
+    let created: String
+    
+}
+
+struct RMOrigin : Codable {
+    let name: String
+    let url: String
+    
+}
+
+struct SingleLocation : Codable {
+    let name: String
+    let url: String
+}
+
+enum RMCharacterStatus : String,Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
+
+enum RmCharacterGender : String,Codable {
+     case male = "Male"
+     case female = "Female"
+     case genderless = "Genderless"
+     case unknown = "unknow"
+    
+}
